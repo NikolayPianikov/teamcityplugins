eb3cf6086d2ad69f910fb58bc132bd7e7c79c49b
Nikolay Pianikov <NikolayP@live.com>
2015-08-14 16:35:35 +0300

    Fix build

---
+++ dotMemoryUnit/dotMemoryUnit-common/src/main/java/jetbrains/buildServer/dotMemoryUnit/Constants.java
@@ -0,0 +1,6 @@
+package jetbrains.buildServer.dotMemoryUnit;
+
+public class Constants {
+    public static final String NUNIT_USE_DOT_MEMORY_UNIT = "nunit_use_dot_memory_unit";
+    public static final String NUNIT_DOT_MEMORY_UNIT_PATH = "nunit_dot_memory_unit_path";
+}
